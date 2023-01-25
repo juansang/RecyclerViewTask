9a3cc2e810dbd0eda71f6f8ce504a60a84c2b616
juansanchezg <jsanchezgayoso@cifpfbmoll.eu>
2023-01-26 00:39:21 +0100

    RecyclerView 2.0.0

--- app/src/main/java/com/example/recyclerviewtask/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewtask/MainActivity.java
@@ -1,25 +1,54 @@
 package com.example.recyclerviewtask;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.LinkedList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity  extends AppCompatActivity {
 
-    private final LinkedList<String> mWordList = new LinkedList<>();
-    private int mCount = 0;
+    private LinkedList<String> mWordList;
+    private int wordListSize;
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+    private FloatingActionButton floatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mWordList = new LinkedList<>();
+        wordListSize = 0;
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        mAdapter = new WordListAdapter(this,mWordList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+
         for (int i = 0; i < 20; i++) {
-            mWordList.addLast("Word " + mCount++);
-            System.out.println("WordList " + mWordList.getLast());
+            mWordList.addLast("Word " + wordListSize++);
         }
+
+
+        floatButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+
+        floatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWordList.add(" + Word " + String.valueOf(wordListSize));
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+                mRecyclerView.smoothScrollToPosition(wordListSize);
+                wordListSize++;
+            }
+        });
     }
 }
