ca853c4bb7d3292b5ca87be3ce1ea3758f28704b
juansanchezg <jsanchezgayoso@cifpfbmoll.eu>
2023-01-27 10:55:29 +0100

    RecyclerView 3.0.0

--- app/src/main/java/com/example/recyclerviewtask/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewtask/MainActivity.java
@@ -14,7 +14,7 @@ import java.util.LinkedList;
 public class MainActivity  extends AppCompatActivity {
 
     private LinkedList<String> mWordList;
-    private int wordListSize;
+    private int mwordListSize;
     private RecyclerView mRecyclerView;
     private WordListAdapter mAdapter;
     private FloatingActionButton floatButton;
@@ -25,17 +25,17 @@ public class MainActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mWordList = new LinkedList<>();
-        wordListSize = 0;
+        mwordListSize = 0;
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 
         mAdapter = new WordListAdapter(this,mWordList);
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+        mRecyclerView.getAdapter().notifyItemInserted(mwordListSize);
 
         for (int i = 0; i < 20; i++) {
-            mWordList.addLast("Word " + wordListSize++);
+            mWordList.addLast("Word " + mwordListSize++);
         }
 
 
@@ -44,10 +44,10 @@ public class MainActivity  extends AppCompatActivity {
         floatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mWordList.add(" + Word " + String.valueOf(wordListSize));
-                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
-                mRecyclerView.smoothScrollToPosition(wordListSize);
-                wordListSize++;
+                mWordList.add(" + Word " + String.valueOf(mwordListSize));
+                mRecyclerView.getAdapter().notifyItemInserted(mwordListSize);
+                mRecyclerView.smoothScrollToPosition(mwordListSize);
+                mwordListSize++;
             }
         });
     }
